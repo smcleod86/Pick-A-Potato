0f747cb29bec15822e0c959c14d21435e1b39c1a
smcleod86 <mcleod.scott_86@yahoo.com>
2020-04-13 09:07:13 -0700

    getting closer

--- js/script.js
+++ js/script.js
@@ -1,91 +1,88 @@
-
+// Global variables
 let time = 30
 let interval
 let score = 0
-let popUpTimer = null
-let potato = null
-let lastPotatoNumber = null
+let potatoes = []
 let potatoImg = document.getElementsByClassName('potato')
+let prompt = document.getElementById('prompt')
+let timer = document.getElementById('timer')
+let start = document.getElementById('start-button')
+let scoreBoard = document.getElementById('score-out')
+let box = document.querySelectorAll('.box')
 let timeUp = false
 
 // DOM loads, add event handler for start, reset buttons.
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('start-button').addEventListener('click', startGame)
-    document.getElementById('timer').textContent = time
-    document.getElementById('score-out').textContent = score
-    document.getElementById('prompt').textContent = "Click Start and Grab the Potatoes!"
+    start.addEventListener('click', playGame)
+    timer.textContent = time
+    scoreBoard.textContent = score
+    prompt.textContent = "Click Start and Grab the Potatoes!"
 })
 
 
-//Start the Game
-const startGame = () => {
-    let score = 0
-    timeUp = true
+// Function to start the game
+const playGame = () => {
     clearInterval(time)
     clearInterval(interval)
     interval = setInterval(countdown, 1000)
-    document.getElementById('start-button').textContent = 'Restart Game'
-    document.getElementById('timer').textContent = time
-    document.getElementById('score-out').textContent = score
-    randomPotato(potatoes)
+    // Reset the text of the start button to reset
+    start.textContent = 'Restart Game'
+    // Show the score
+    scoreBoard.textContent = score
+    randomPotato(potatoImg)
     popUp()
-    grabPotato()
-    grab()
+    // grabPotato()
+    // grab()
 }
 
 
 // Countdown function
 const countdown = () => {
-    console.log("countdown", time)
+    // Timer decreases
     time--
-    document.getElementById('prompt').textContent = "Get the Potatoes!"
-    document.getElementById('timer').textContent = time
+    // Change text in text box
+    prompt.textContent = "Get the Potatoes!"
+    // Update the timer
+    timer.textContent = time
+    // When time is up, run game over function
     if (time == 0) {
-        document.getElementById('prompt').textContent = "GAME OVER!"
+        prompt.textContent = "GAME OVER!"
         gameOver()
-        }
     }
-
-
-// Pop up function
-const popUp = () => {
-    let minUpTime = 1200
-    let maxUpTime = 2500
-    let time = randomTime(minUpTime, maxUpTime)
-    randomPotato()
-    potatoes = document.getElementsByClassName('potato')
-    document.getElementsByClassName('potato')
-    if (potatoes) {
-        for (let i = 0; i < potatoes.length; i++) {
-            if (timeUp === false) {
-                potatoes[i].style.visibility = "visible"
-            }
-        }
-    }
-    popUpTimer = setTimeout(() => {
-        document.getElementsByClassName('potato')
-        if (potatoes) {
-            for (let i = 0; i < potatoes.length; i++) {
-                potatoes[i].style.visibility = "hidden"
-            }
-        }
-        if (timeUp === false) {
-            popUp()
-        }
-    }, time)
 }
 
 
+// // Pop up function
+// const popUp = () => {
+//     let minUpTime = 1200
+//     let maxUpTime = 2500
+//     let popTime = randomTime(minUpTime, maxUpTime)
+//     randomPotato()
+//     if (potatoes) {
+//         for (let i = 0; i < potatoes.length; i++) {
+//             if (timeUp === false) {
+//                 potatoes[i].hidden = "false"
+//             }
+//         }
+//     }
+// }
+
+
+const popUp = (randomTime) => {
+    setTimeout(() => {
+       randomPotato(potatoImg)
+    }, randomTime)
+}
+
 // Grab potato function
 const grab = () => {
-    potatoes = document.querySelectorAll('.potato')
+    let potatoes = document.querySelectorAll('.potato')
     if (potatoes) {
         for (let i = 0; i < potatoes.length; i++) {
            potatoes[i].addEventListener('click', grab)
         }
     }
     score++
-    document.getElementById('score-out')
 }
 
 
@@ -98,41 +95,49 @@ const randomTime = (min, max) => {
    
 
 // Randomize potato to display Function
-const randomPotato = (potatoes) => {
-    let x = Math.floor(Math.random() * lastPotatoNumber)
-    let potato = potatoes * (x)
-    if (x === lastPotatoNumber) {
-        return randomPotato(potatoes)
-    }
-    lastPotatoNumber = x
-    return potato
+const randomPotato = () => {
+    box.forEach(className => {
+        className.classList.remove('potatoImg')
+    })
+    let position = Math.floor(Math.random() * 12) 
+        position.classList.add('potatoImg')
+    //potatoImg[x].style.visibility = 'visible';
+
+    hitPosition = position.id
 }
 
 
 // Function to add click event
-const grabPotato = () => {
-    let potatoImg = document.getElementsByClassName('.potato')
-    // Loop through each
-    for (let i = 0; i < potatoImg.length; i++) {
-        potatoImg.addEventListener('click', grab)
-    }
-}
+box.forEach(id => {
+    id.addEventListener('click', () => {
+        if(id.id === hitPosition) {
+            score = score + 1
+            scoreBoard.textContent = score
+        }
+    })
+})
+// const grabPotato = () => {
+//     // Loop through each
+//     for (let i = 0; i < potatoImg.length; i++) {
+//         potatoImg.addEventListener('click', grab)
+//     }
+// }
 
 
-// Function to remove click events
-const removeGrabPotato = () => {
-    let potatoImg = document.getElementsByClassName('.potato')
-        // Loop through each
-    for (let i = 0; i < potatoImg.length; i++) {
-        potatoImg.removeEventListener('click', removeGrabPotato)
-    }   
-}
+// // Function to remove click events
+// const removeGrabPotato = () => {
+//     let potatoImg = document.getElementsByClassName('.potato')
+//         // Loop through each
+//     for (let i = 0; i < potatoImg.length; i++) {
+//         potatoImg.removeEventListener('click', removeGrabPotato)
+//     }   
+// }
 
 
 // Game Over Function
 const gameOver = () => {
     clearInterval(interval)
-    clearInterval(popUpTimer)
+    clearInterval(popUp)
     timeUp = true
     //removeGrabPotato()
 }
